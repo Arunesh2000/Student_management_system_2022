ed0c9f22e0a12b3e7f0db5ef66ee84cbd6bf56f1
Arunesh2000 <77392923+Arunesh2000@users.noreply.github.com>
2022-05-24 18:37:02 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+ <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      Student Management System
+    </div>
+    <!-- Default to the left -->
+    <strong>Arunesh Kumar tiwari &copy; 2022.</strong> All rights reserved.
+  </footer>
